3ec32f410748e09b1065f7f589edd01d4ca7a2a8
kishimoto <drehbleistift@gmail.com>
2022-01-14 15:48:25 +0900

    リンク

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, Link } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/system";
 import React from "react";
@@ -15,27 +15,41 @@ const Header = ({ filter, setActiveNode, setFilter, searchOptions }) => {
   };
 
   return (
-    <Stack
-      direction="row"
-      spacing={1}
-      sx={{ p: "12px", justifyContent: "center" }}
-    >
-      <Filter
-        data={searchOptions}
-        filter={filter}
-        setFilter={setFilter}
-        placeholder={"お笑い芸人"}
-      />
-      <StyledButton variant="contained" onClick={handleResetClick} size="small">
-        リセット
-      </StyledButton>
-      {/* <StyledButton
+    <Stack direction="column">
+      <Stack
+        direction="row"
+        spacing={1}
+        sx={{ pt: "12px", justifyContent: "center" }}
+      >
+        <Filter
+          data={searchOptions}
+          filter={filter}
+          setFilter={setFilter}
+          placeholder={"お笑い芸人"}
+        />
+        <StyledButton
+          variant="contained"
+          onClick={handleResetClick}
+          size="small"
+        >
+          リセット
+        </StyledButton>
+        {/* <StyledButton
         variant="contained"
         onClick={handleAxisChangeClick}
         size="small"
       >
         水平/垂直
       </StyledButton> */}
+      </Stack>
+      <Link
+        href="https://twitter.com/unpuy_tw"
+        underline="hover"
+        variant="subtitle2"
+        sx={{ color: "#f4f4f4" }}
+      >
+        @unpuy_tw
+      </Link>
     </Stack>
   );
 };
